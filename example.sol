02638c8a1779b85507359754b02a00356e6204c8
ethanw2457 <ethan.w2457@gmail.com>
2024-09-24 22:23:12 -0400

    Initial

---
+++ example.sol
@@ -0,0 +1,49 @@
+pragma solidity ^0.8.26; // states the version of the file, the '^' means any future version
+// no null values in solidity, only default values
+// int default value is = 0
+// string default value is = ""
+contract example { // Usually name the contract the same as the file name 
+
+    string name= "example"; // Note that string is lowercase 
+    bool exists = true;
+
+    uint public num1 = 1;// allow for solidity to specify the size, uint can not be negative, initialized as private but can be changd to public
+    // private variables are stored within the smartcontract and can not be accessed directly
+    uint256 num2 = 1;// specify the size yourself by adding a number (256 is the biggest possible value)
+    // You need to recompile everytime you make a change
+    int num3 = -1; 
+
+    address owner; // Usually declare the address variable and then define it later on in the code
+
+    struct myStruct { // object of multiple variables, defines what myStruct is, syntax for creating a struct
+        uint256 num; // contains num and string test
+        string test;
+    } 
+
+    myStruct public struct1; // Declare the variable 
+
+    myStruct[] public structs; // initialize an array of structs
+
+    function changeStructs(uint256 n, string memory ntest) public {
+        
+        struct1.num = n; // calls the struct variable before changing it to the parameter
+        struct1.test = ntest; 
+        structs.push(struct1); // pushing a copy of struct1 into the myStruct array like a stack
+    }
+
+    function changeNum(uint256 a) public {
+        num1 = a;
+        require(a > 18); // if the require statement fails it will reverse the entire transaction
+    }
+
+    function getNum() view public returns (uint256) { // returns the type which is in the parenthesis
+        return num1;
+    }
+
+    // function add(uint x, uint y) pure {
+
+    // }
+
+
+
+}
