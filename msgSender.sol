4e7e518fc7f92778b42db2cad5ab8a0d2140bc88
ethanw2457 <ethan.w2457@gmail.com>
2024-10-15 18:26:50 -0400

    Update

--- msgSender.sol
+++ msgSender.sol
@@ -4,13 +4,16 @@ pragma solidity ^0.8;
 contract msgSender {
     
     address public lastUser;
+    int num;
 
-    // address owner;
-    // constructor() {
-    //    owner = msg.sender;
-    // }
+    address owner;
+    constructor() {
+       owner = msg.sender;
+       num = 5;
+    }
 
     function setLastUser() public {
+        require(msg.sender == owner);
         lastUser = msg.sender;
     }
 }
