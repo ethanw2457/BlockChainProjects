271712c5c450bea07c0a79bf2b8db4692680b288
ethanw2457 <ethan.w2457@gmail.com>
2024-10-29 18:20:01 -0400

    Update

--- attendance/attendance.sol
+++ attendance/attendance.sol
@@ -2,5 +2,12 @@
 pragma solidity ^0.8;
 
 contract Attendance {
+    address[] interacted;
+
+    mapping(address => string) addressToName;
+
+    function markAttendance(string name) public {
+        msg.sender;
+    }
 
 }
